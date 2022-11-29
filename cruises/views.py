f5a84f8d715c8d7339360219d3c99c498bded7f2
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2022-11-29 09:01:14 +0000

    cleaned up repo

--- cruises/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Cruise
-
-# Create your views here.
-def index(request):
-    cruises = Cruise.objects.all()
-    return render(request, 'cruises/index.html', {'cruises': cruises})
